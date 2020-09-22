4f6a0fd7151c6c43eab1f7013e7c058ee7b7faf3
Joey Cadieux <58401407+JoeyCreator@users.noreply.github.com>
2020-09-22 11:35:08 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./styles.css";
+import Navbar from "./components/Navbar";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <h1>Data Center Security Tower.</h1>
+      <h2>
+        A data center or data centre is a building, dedicated space within a
+        building, or a group of buildings used to house computer systems and
+        associated components, such as telecommunications and storage systems.
+      </h2>
+    </div>
+  );
+}
